d77a718fd16c072b4a2f393d4972f248e30e49ad
nagalakshmipujari <63413678+nagalakshmipujari@users.noreply.github.com>
2020-04-22 17:18:14 +0530

    Add files via upload

---
+++ Assignment1_Nagalakshmi/case3/case3c.cpp
@@ -0,0 +1,77 @@
+/*program about type of storage classess.
+filename:case3c.cc
+author:Nagalakshmi.P
+date:10-04-2020.*/
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+using namespace std;
+extern int ig;     //storage class extern variable declaration
+
+/*
+function name:storageauto
+return type:int
+parameters:int ia1,int ia2
+*/
+int storageauto(int ia1,int ia2)
+{
+        auto int isum;
+        isum=ia1+ia2;
+        return isum;
+}
+/*
+function name: storagestatic
+return type:void
+function that describes the static storage class
+*/
+
+static int is=5;   //static variable declaration
+void storagestatic()
+{
+        static int i=0;
+        cout<<"static storage::is=\t"<<is--<<endl;
+        cout<<"static storage::i=\t"<<i++<<endl;
+}
+
+/*
+function name: storageregister
+return type:void
+function that describes the register storage class
+*/
+void storageregister()
+{
+        register int roll=35;
+        cout<<"Register Variable roll:"<<roll<<endl;
+}
+/*function name:storageextern
+return type:void
+function that describe the extern storage classes
+*/
+void storageextern()
+{
+        int ig=10;
+cout<<"Extern Storage ::ig is"<<ig<<endl; //display the extern variable
+}
+
+int main(int argc,char* argv[])
+{
+        int ii=2,ij=3,ik,ig=7;
+        if((argc==2)&&(strcmp(argv[1],"-h")==0))
+        {
+        cout<<"usage:: ./a.out"<<endl;
+        cout<<"describe the storage classes"<<endl;
+        }
+else
+{
+//auto storage fucntion calling
+ik=storageauto(ii,ij);
+cout<<"Auto storage :: iSum="<<ik<<endl;
+//calling all storage classes functions
+storageextern();
+storageregister();
+storagestatic();
+cout<<"ig is:"<<ig<<endl;
+return 0;
+}
+}
+
